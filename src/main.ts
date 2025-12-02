ac3fdd648a043b036ae0486704fa35c0624e72ee
Gustavo Costa <cmtehenz@gmail.com>
2025-12-02 10:32:43 -0300

    ajuste cors

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -43,6 +44,9 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
+
+  // Gera o arquivo openapi.json com todas as rotas
+  fs.writeFileSync('./openapi.json', JSON.stringify(document, null, 2));
 
   const port = process.env.PORT || 3333;
   await app.listen(port);
